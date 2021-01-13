b5821bf61385125bc8d4311778c1aeda8ce3f6e7
Hajdukovic <hrvoje.hajdukovic96@gmail.com>
2021-01-13 12:02:35 +0100

    ruta za unos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">   
-	    
+
+
+	 	    <a href="{{ route('unos') }}" class="btn btn-outline-primary" role="button" aria-pressed="true">Dodaj novi film</a>
+   
 	<h1>Popis filmova</h1>
     
 
